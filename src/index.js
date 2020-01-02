82b120c2979e1b84c4ec846533981b61e1cde100
Yana Chernova <yanachernova@MacBook-Air-Yana.local>
2020-01-02 15:08:33 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './index.css';
+
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
