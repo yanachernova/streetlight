aa2a5d634b23fa4fed8363a8b16845d4db047769
Yana Chernova <yanachernova@MacBook-Air-Yana.local>
2020-01-07 13:50:38 -0300

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const styleTop = {
+  backgroundColor: 'Black',
+  width: "1rem",
+  margin: "0 auto",
+  borderRadius: " 0.25rem 0.25rem 0 0"
+
+}
+const styleBody = {
+  backgroundColor: 'Black',
+  width: "6rem",
+  margin: "0 auto",
+  height: "5rem",
+
+}
+const styleButton = {
+  width: "4rem",
+  margin: "0 auto",
+  height: "4rem",
+  borderRadius: "50px",
+
 }
 
-export default App;
+class TrafficLight extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      clickedLight: 'yellow'
+    };
+  }
+  render() {
+    console.log(this.state);
+    let redExtraClass = '';
+    //if (this.state.clickedLight == 'red') redExtraClass = 'selected'; else redExtraClass = '';
+
+    let yellowExtraClass = '';
+    //if (this.state.clickedLight == 'yellow') yellowExtraClass = 'selected'; else yellowExtraClass = '';
+
+    let greenExtraClass = '';
+    //if (this.state.clickedLight == 'green') greenExtraClass = 'selected'; else greenExtraClass = '';
+
+    return (
+      <div className="container">
+        <div className="card" style={styleTop}>
+          <div className="card-body">
+          </div>
+        </div>
+
+        <div className="card" style={styleBody}>
+          <div className="card-body pl-3 pt-2">
+            <div className={"btn btn-danger red " + (this.state.clickedLight === "red" ? 'selected' : '' )} style={styleButton} onClick={() => this.setState({ clickedLight: "red" })}></div>
+          </div>
+        </div>
+
+        <div className="card text-right" style={styleBody}>
+          <div className="card-body pl-3 pt-2">
+            <div className={"btn btn-warning yellow " + (this.state.clickedLight === 'yellow' ? 'selected' : '' )} style={styleButton} onClick={() => this.setState({ clickedLight: "yellow" })}> </div>
+          </div>
+        </div>
+
+        <div className="card text-right" style={styleBody}>
+          <div className="card-body pl-3 pt-1">
+            <div className={"btn btn-success green " + (this.state.clickedLight === 'green' ? 'selected' : '' )} style={styleButton} onClick={() => this.setState({ clickedLight: "green" })}></div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default TrafficLight;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TrafficLight from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.css';
@@ -10,7 +10,7 @@ import 'jquery';
 import 'popper.js';
 import 'bootstrap';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TrafficLight />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
